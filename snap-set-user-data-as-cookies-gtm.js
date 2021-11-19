93b720411150226e3175760f25d8246e701c6a70
Armin <armin@mashmetrics.com>
2021-11-19 00:33:11 -0600

    adding javascript file to drop cookies of form values

--- snap-set-user-data-as-cookies-gtm.js
+++ snap-set-user-data-as-cookies-gtm.js
@@ -1,74 +1,79 @@
 /*
  * Add listeners for form input fields
  */
-
-var formInputFields = [
-  {"name": "First Name"},
-  {"id": "lastName"},
-  {"name": "Mobile Phone Number"},
-  {"id": "emailAddress"},
-  {"id": "verificationCode"},
-  {"id": "homeAddress"},
-  {"name": "Zip Code"},
-  {"id": "apartment/suite"},
-  {"id": "dateOfBirth"},
-  {"id": "driverLicenseNumber"},
-  {"id": "state"},
-  {"id": "socialSecurityNumberOrItin"},
-];
-
-for (var i = 0; i < formInputFields.length; i++) {
-  var formElementObject = formInputFields[i];
-  var elementAttributeName = Object.keys(formElementObject);
-  var elementAttributeValue = formElementObject[elementAttributeName];
-  var elementQuerySelector = "[" + elementAttributeName + "='" + elementAttributeValue +"']";
-  var formInputField = document.querySelector(elementQuerySelector);
-  if (formInputField !== null) {
-    console.log("Adding listener for element matching querySelector(" + elementQuerySelector + ")");
-    formInputField.addEventListener("focusout", function(event) {
-      var eventElementId = event.target.id;
-      var eventElementName = event.target.name;
-      var eventElementValue = event.target.value;
-      console.log("eventElementId: " + eventElementId + " | eventElementName: " + eventElementName + " | eventElementValue: " + eventElementValue);
-    });
+function addListenersForFormInputFields() {
+  var formInputFields = [
+    {"name": "First Name"},
+    {"id": "lastName"},
+    {"name": "Mobile Phone Number"},
+    {"id": "emailAddress"},
+    {"id": "verificationCode"},
+    {"id": "homeAddress"},
+    {"name": "Zip Code"},
+    {"id": "apartment/suite"},
+    {"id": "dateOfBirth"},
+    {"id": "driverLicenseNumber"},
+    {"id": "state"},
+    {"id": "socialSecurityNumberOrItin"},
+  ];
+  
+  for (var i = 0; i < formInputFields.length; i++) {
+    var formElementObject = formInputFields[i];
+    var elementAttributeName = Object.keys(formElementObject);
+    var elementAttributeValue = formElementObject[elementAttributeName];
+    var elementQuerySelector = "[" + elementAttributeName + "='" + elementAttributeValue +"']";
+    var formInputField = document.querySelector(elementQuerySelector);
+    if (formInputField !== null) {
+      console.log("Adding listener for element matching querySelector(" + elementQuerySelector + ")");
+      formInputField.addEventListener("focusout", function(event) {
+        var eventElementId = event.target.id;
+        var eventElementName = event.target.name;
+        var eventElementValue = event.target.value;
+        console.log("eventElementId: " + eventElementId + " | eventElementName: " + eventElementName + " | eventElementValue: " + eventElementValue);
+      });
+    }
   }
 }
+addListenersForFormInputFields()
 
 /*
  * Add listeners for form buttons
  */
 
-var formButtonsObject = {
-  "rentOrOwn": [
-    "rentbtn",
-    "ownbtn",
-  ],
-  "yearsAtThisAddress": [
-    "lessthanonebtn",
-    "onetothreebtn",
-    "threetofivebtn",
-    "fiveormorebtn",
-  ]
-};
+function addListenersForFormButtons() {
+  var formButtonsObject = {
+    "rentOrOwn": [
+      "rentbtn",
+      "ownbtn",
+    ],
+    "yearsAtThisAddress": [
+      "lessthanonebtn",
+      "onetothreebtn",
+      "threetofivebtn",
+      "fiveormorebtn",
+    ]
+  };
 
-var formButtonsObjectKeys = Object.keys(formButtonsObject);
-for (var i = 0; i < formButtonsObjectKeys.length; i++) {
-  var formButtonObjectKeyName = formButtonsObjectKeys[i];
-  var formButtonObjectKeyIds = formButtonsObject[formButtonObjectKeyName];
-  for (var ii = 0; ii < formButtonObjectKeyIds.length; ii++) {
-    var formButton = document.getElementById(formButtonObjectKeyIds[ii]);
-    if (formButton !== null) {
-      formButton.addEventListener("click", function(event) {
-        var eventId = event.target.id;
-        var formButtonsObjectKeys = Object.keys(formButtonsObject);
-        for (var i = 0; i < formButtonsObjectKeys.length; i++) {
-          var formButtonObjectKeyName = formButtonsObjectKeys[i];
-          var formButtonObjectKeyIds = formButtonsObject[formButtonObjectKeyName];
-          if (formButtonObjectKeyIds.includes(eventId)) {
-            console.log(formButtonObjectKeyName + ": " + eventId);
+  var formButtonsObjectKeys = Object.keys(formButtonsObject);
+  for (var i = 0; i < formButtonsObjectKeys.length; i++) {
+    var formButtonObjectKeyName = formButtonsObjectKeys[i];
+    var formButtonObjectKeyIds = formButtonsObject[formButtonObjectKeyName];
+    for (var ii = 0; ii < formButtonObjectKeyIds.length; ii++) {
+      var formButton = document.getElementById(formButtonObjectKeyIds[ii]);
+      if (formButton !== null) {
+        formButton.addEventListener("click", function(event) {
+          var eventId = event.target.id;
+          var formButtonsObjectKeys = Object.keys(formButtonsObject);
+          for (var i = 0; i < formButtonsObjectKeys.length; i++) {
+            var formButtonObjectKeyName = formButtonsObjectKeys[i];
+            var formButtonObjectKeyIds = formButtonsObject[formButtonObjectKeyName];
+            if (formButtonObjectKeyIds.includes(eventId)) {
+              console.log(formButtonObjectKeyName + ": " + eventId);
+            }
           }
-        }
-      });
+        });
+      }
     }
   }
 }
+addListenersForFormButtons();
